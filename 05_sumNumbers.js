95525b0270803cb1c92c5be8fd9d01310e6cf3a8
carlmccabe <carl.h.mccabe@gmail.com>
2019-09-09 10:09:51 +1000

    05

--- 05_sumNumbers.js
+++ 05_sumNumbers.js
@@ -2,11 +2,24 @@
 // and return the name of the player with the highest total score.
 // Test your solution:
 // npm test
-
+function total(arr) {
+  // calculates the total of the array of scores that is passed in.
+  let totalScore = 0;
+  for (let num of arr) {
+    totalScore += num;
+  }
+  return totalScore;
+}
 function findWinner(players) {
-    // Your code here
+  let winner = players[0];
+  for (let ind = 1; ind < players.length; ind++) {
+    if (total(players[ind].scores) > total(winner.scores)) {
+      winner = players[ind];
+    }
+  }
+  return winner.name;
 }
 
 module.exports = {
-    findWinner
-}
+  findWinner
+};
